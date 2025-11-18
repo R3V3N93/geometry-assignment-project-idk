eb8f51a198304f035ab3b639a8c68e4f9cf2638e
R3V3N93 <123841546+R3V3N93@users.noreply.github.com>
2025-11-18 10:00:00 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace geovector
+{
+    class Program
+    {
+        public class Vector
+        {
+            public double direction;
+            public double magnitude;
+
+            public Vector2 ToVector2()
+            {
+                Vector2 temp = new Vector2(1, 1);
+                return temp;
+            }
+        }
+
+        public class Vector2
+        {
+            public double x;
+            public double y;
+
+            public Vector2(double X, double Y)
+            {
+                this.x = X;
+                this.y = Y;
+            }
+
+            public Vector ToVector()
+            {
+                Vector temp = new Vector();
+                return temp;
+            }
+
+            public static Vector2 operator +(Vector2 a, Vector2 b)
+            {
+                return new Vector2(a.x + b.x, a.y + b.y);
+            }
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}   
